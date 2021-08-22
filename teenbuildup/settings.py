22525292fb9d482f9beddc3186903f263426822b
Times4Everyone <76495035+Times4Everyone@users.noreply.github.com>
2021-08-21 21:20:32 -0500

    Update settings.py

--- teenbuildup/settings.py
+++ teenbuildup/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '=_agbczi48eu6)1n@y=(pac6%d2wo(l^8tpo)mpp4j4%he5zl^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['teenbuildup.pythonanywhere.com']
 
