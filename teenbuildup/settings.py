d63e80c0e8ba26cb3667e1c01dc2a286e1ef07ca
Times4Everyone <76495035+Times4Everyone@users.noreply.github.com>
2021-09-04 00:42:17 -0500

    Update settings.py

--- teenbuildup/settings.py
+++ teenbuildup/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = '=_agbczi48eu6)1n@y=(pac6%d2wo(l^8tpo)mpp4j4%he5zl^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['ec2-3-85-102-216.compute-1.amazonaws.com']
+ALLOWED_HOSTS = ['ec2-23-23-24-121.compute-1.amazonaws.com']
 
 
 # Application definition
